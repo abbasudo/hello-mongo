a888f4c5e08b6159b3240d73b9918a61c4b1dea3
Abbas mkhzomi <llabbasmkhll@gmail.com>
2023-02-18 19:53:56 +0330

    add categories route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
 
 Route::apiResource('posts', PostController::class);
+Route::apiResource('categories', CategoryController::class);
