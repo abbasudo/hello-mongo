30d38fbe45f9ab78a650803b24f8983672e31045
Abbas mkhzomi <llabbasmkhll@gmail.com>
2023-02-18 18:02:22 +0330

    add basic post index and create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use App\Models\Post;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response as Status;
 
 class PostController extends Controller
 {
+    /**
+     * get list of all posts
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $posts = Post::all();
 
+        return PostResource::collection($posts)->response()->setStatusCode(Status::HTTP_OK);
+    }
+
+    /**
+     * Store a new post
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $attributes = $request->validate([
+            'slug'  => 'required|string|max:255',
+            'title' => 'required|string|max:255',
+        ]);
+
+        $post = Post::create($attributes);
+
+        return (new PostResource($post))->response()->setStatusCode(Status::HTTP_CREATED);
+    }
 }
